5f2fd9bbd062d52b97565c1944a03f8d69b4d583
Ole Skyggebjerg <ole@skyggebjerg.dk>
2025-03-25 15:19:32 +0100

    mod

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,33 @@ int en_pin = 22; // enable pin
 const byte stepPin = 19;
 const byte dirPin = 23;
 
-const int stepsPerRev = 200;    // Steps per revolution - may need to be adjusted
+const int stepsPerRev = 200*32;    // Steps per revolution - 1/32 microstepping
 
 MoToStepper stepper1( stepsPerRev, STEPDIR );  // create a stepper instance
 
 void setup() {
   pinMode(en_pin, OUTPUT);
+  pinMode(32, INPUT_PULLUP);
   digitalWrite(en_pin, LOW); // enable driver
   stepper1.attach( stepPin, dirPin );
-  stepper1.setSpeed( 300 );              // 30 rev/min (if stepsPerRev is set correctly)
-  stepper1.setRampLen( stepsPerRev / 2); // Ramp length is 1/2 revolution
-  stepper1.rotate(1);                    // start turning, 1=vorward, -1=backwards                    
+  stepper1.setSpeed( 600 );              // 30 rev/min (if stepsPerRev is set correctly)
+  stepper1.setRampLen( stepsPerRev / 4); // Ramp length is 1/2 revolution
+  //stepper1.rotate(1);                    // start turning, 1=vorward, -1=backwards                    
+  //delay(10000);
+  //stepper1.stop();                       // stop turning
+  //stepper1.rotate(0);                    // stop turning
+  //delay(2000);
+  stepper1.doSteps( stepsPerRev *4);        // do one revolution
+  //delay(10000);
+  //stepper1.doSteps( -stepsPerRev );
+
+  //delay(20000);
+  //digitalWrite(en_pin, HIGH); // disable driver
 }
 
 void loop() {
+  if(digitalRead(32) == LOW){ 
+    stepper1.stop();
+    digitalWrite(en_pin, HIGH); // disable driver
+  }
 }
